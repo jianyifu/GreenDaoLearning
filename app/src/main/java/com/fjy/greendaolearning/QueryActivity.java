6c0a9577a806caf9c22c293a29b75082a27392a4
fjy_sogou_pc <jianyifu@gmail.com>
2017-05-05 15:10:51 +0800

    Executing Queries multiple times with different params

--- app/src/main/java/com/fjy/greendaolearning/QueryActivity.java
+++ app/src/main/java/com/fjy/greendaolearning/QueryActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,14 +13,18 @@ import com.fjy.greendaolearning.db.PushMsgDao;
 import com.fjy.greendaolearning.pojo.PushMsg;
 
 import org.greenrobot.greendao.query.Query;
+import org.greenrobot.greendao.query.QueryBuilder;
 
 import java.util.List;
 
 public class QueryActivity extends AppCompatActivity {
+    private static final String TAG = "QueryActivity";
     private Button doQueryBtn;
     private RecyclerView msgList;
     private PushMsgAdapter adapter;
     private PushMsgDao pushMsgDao;
+    private Query<PushMsg> msgQuery;
+    private Button doQueryAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +32,13 @@ public class QueryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_query);
         setUpViews();
         pushMsgDao = DBManager.getInstance().getDaoSession().getPushMsgDao();
+        QueryBuilder<PushMsg> qb = pushMsgDao.queryBuilder();
+        msgQuery = qb.where(PushMsgDao.Properties.Title.like("aa%")).offset(0).limit(20).build();
     }
 
     private void setUpViews() {
         doQueryBtn = (Button) findViewById(R.id.do_query_btn);
+        doQueryAgain = (Button) findViewById(R.id.do_query_again);
         msgList = (RecyclerView) findViewById(R.id.msg_list);
         msgList.setLayoutManager(new LinearLayoutManager(this));
         adapter = new PushMsgAdapter(new PushMsgAdapter.MsgClickListener() {
@@ -43,21 +51,28 @@ public class QueryActivity extends AppCompatActivity {
         doQueryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               doLimitOffsetQuery();
+                doLimitOffsetQuery();
             }
-
-
+        });
+        doQueryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doLimitOffsetQueryWithDiffParams();
+            }
         });
     }
 
+    private void doLimitOffsetQueryWithDiffParams() {
 
-
+        msgQuery.setParameter(0, "cc%");
+        List<PushMsg> msgs = msgQuery.list();
+        Log.d(TAG, "doLimitOffsetQuery() called msgQuery"+msgQuery);
+        adapter.setMsgs(msgs);
+    }
 
 
     private void doLimitOffsetQuery() {
         //get items from index=2 and count=5
-       Query<PushMsg> msgQuery =  pushMsgDao.queryBuilder().limit(5).offset(2).build();
-        List<PushMsg> msgs = msgQuery.list();
-        adapter.setMsgs(msgs);
+        adapter.setMsgs(msgQuery.list());
     }
 }
